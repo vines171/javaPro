88291d14a87f34b6bb23d041e9c6815a9c0facac
vines <vines171@mail.ru>
2025-09-16 22:43:17 +0300

    каждый тест с Привет!

--- src/main/java/TestRunner2.java
+++ src/main/java/TestRunner2.java
@@ -3,6 +3,7 @@ import enums.TestResult;
 import exceptions.BadTestClassError;
 import exceptions.TestAssertionError;
 import lombok.SneakyThrows;
+import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.InvocationHandler;
 
 import java.lang.annotation.Annotation;
@@ -14,13 +15,17 @@ import java.util.*;
 import static java.util.stream.Collectors.toList;
 
 public class TestRunner2
-        implements InvocationHandler
-{
+        implements InvocationHandler {
     static Object object;
 
     public TestRunner2(Object object) {
         this.object = object;
     }
+
+    Method[] methods = TestSuccess.class.getDeclaredMethods();
+    List<Method> beforeSuiteMethods = getMethodsByType(methods, BeforeSuite.class);
+    List<Method> afterSuiteMethods = getMethodsByType(methods, AfterSuite.class);
+
 
     private static String getTestName(Method method) {
         String nameValue = method.getDeclaredAnnotation(Test.class).name();
@@ -55,16 +60,22 @@ public class TestRunner2
             List<Method> testList = getMethodsByType(methods, Test.class);
 
 
+//            TestSuccess testSuccess1 = new TestSuccess();
+//            TestSuccess testSuccess = (TestSuccess) Enhancer.create(TestSuccess.class, new TestRunner2(testSuccess1));
+
+//            testSuccess.beforeAllTests();
+//            testSuccess.test1();
 
             // Выполнение BeforeSuite методов
-            executeMethods(beforeSuiteMethods, null);
+//            executeMethods(beforeSuiteMethods, null);
 
             for (Method testMethod : testList) {
                 executeSingleTest(testMethod, testInstance, beforeEachMethods, afterEachMethods, results);
             }
 
             // Выполнение AfterSuite методов
-            executeMethods(afterSuiteMethods, null);
+//            executeMethods(afterSuiteMethods, null);
+//            testSuccess.afterAllTests();
 
         } catch (Exception e) {
             throw new BadTestClassError("Failed to execute tests: " + e.getMessage());
@@ -76,6 +87,9 @@ public class TestRunner2
     private static void executeSingleTest(Method testMethod, Object testInstance,
                                           List<Method> beforeEachMethods, List<Method> afterEachMethods,
                                           Map<TestResult, List<TestInfo>> results) {
+
+        TestSuccess testSuccess1 = new TestSuccess();
+        TestSuccess testSuccess = (TestSuccess) Enhancer.create(TestSuccess.class, new TestRunner2(testSuccess1));
 
         String testName = getTestName(testMethod);
         TestResult testResult = TestResult.SUCCESS;
@@ -92,8 +106,9 @@ public class TestRunner2
             executeMethods(beforeEachMethods, testInstance);
 
 //             Выполнение теста
+
             testMethod.setAccessible(true);
-            testMethod.invoke(testInstance);
+            testMethod.invoke(testSuccess);
 
         } catch (InvocationTargetException e) {
             Throwable cause = e.getCause();
@@ -127,7 +142,7 @@ public class TestRunner2
     private static void executeMethods(List<Method> methods, Object instance) {
         for (Method method : methods) {
             method.setAccessible(true);
-            if ( Modifier.isStatic(method.getModifiers())) {
+            if (Modifier.isStatic(method.getModifiers())) {
                 method.invoke(null);
             } else {
                 method.invoke(instance);
@@ -135,9 +150,41 @@ public class TestRunner2
         }
     }
 
+//    @SneakyThrows
+//    @Override
+//    public Object invoke(Object proxy, Method method, Object[] args) {
+//        return method.invoke(object, args);
+//    }
+
+
     @SneakyThrows
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) {
-        return method.invoke(object, args);
+//        Object obj = method.invoke(object, args);
+
+
+        if(method.isAnnotationPresent(Test.class)) {
+            System.out.println("Привет!");
+
+//            method.invoke(beforeSuiteMethods, args);
+            method.invoke(object, args);
+//            method.invoke(afterSuiteMethods, args);
+
+        }
+
+
+        return null;
+
+
+
+//        TestSuccess.beforeAllTests();
+//        if (method.isAnnotationPresent(Test.class)) {
+//            testSuccess.beforeEachTest();
+//            method.invoke(object, args);
+//            testSuccess.afterEachTest();
+//        }
+//        TestSuccess.afterAllTests();
+//        return method.invoke(object, args);
+//                method.invoke(object, args);
     }
 }
--- src/main/java/TestSuccess.java
+++ src/main/java/TestSuccess.java
@@ -30,23 +30,23 @@ public class TestSuccess {
         System.out.println("Running successful test");
     }
 
-    @Test(name = "Failed test", priority = 5)
-    void failedTest() {
-        System.out.println("Running failed test");
-        throw new TestAssertionError("Test condition failed");
-    }
+//    @Test(name = "Failed test", priority = 5)
+//    void failedTest() {
+//        System.out.println("Running failed test");
+//        throw new TestAssertionError("Test condition failed");
+//    }
 
     @Test(name = "Test 1", priority = 1)
     void test1(){
         System.out.println("Первый тест");
     }
 
-    @Test(name = "Test TestAssertionError", priority = 2)
-    @Order(value = 1)
-    void testAssertionError() {
-        System.out.println("run testAssertionError");
-        throw new TestAssertionError("Ошибка");
-    }
+//    @Test(name = "Test TestAssertionError", priority = 2)
+//    @Order(value = 1)
+//    void testAssertionError() {
+//        System.out.println("run testAssertionError");
+//        throw new TestAssertionError("Ошибка");
+//    }
     @Test(name = "High priority test", priority = 10)
     void testHighPriority() {
         System.out.println("Running high priority test");
@@ -68,8 +68,8 @@ public class TestSuccess {
         System.out.println("This test should be skipped");
     }
 
-    @Test(name = "Unexpected error")
-    void testWithError() {
-        throw new RuntimeException("Unexpected error");
-    }
+//    @Test(name = "Unexpected error")
+//    void testWithError() {
+//        throw new RuntimeException("Unexpected error");
+//    }
 }
